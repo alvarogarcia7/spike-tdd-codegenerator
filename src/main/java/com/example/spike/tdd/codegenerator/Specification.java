86cf67e7b1d28cf20729f0ed92ddf8d8857a5f06
alvaro garcia <derecerca@gmail.com>
2015-06-13 16:10:21 +0200

    refactor: move hash,equals,toString to the end of the file

--- src/main/java/com/example/spike/tdd/codegenerator/Specification.java
+++ src/main/java/com/example/spike/tdd/codegenerator/Specification.java
@@ -20,38 +20,6 @@ class Specification {
 		this.testCode = testCode;
 	}
 
-	@Override
-	public boolean equals (final Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Specification)) return false;
-
-		final Specification that = (Specification) o;
-
-		if (name != null ? !name.equals(that.name) : that.name != null) return false;
-		if (productionCode != null ? !productionCode.equals(that.productionCode) : that.productionCode != null)
-			return false;
-		if (testCode != null ? !testCode.equals(that.testCode) : that.testCode != null) return false;
-
-		return true;
-	}
-
-	@Override
-	public int hashCode () {
-		int result = name != null ? name.hashCode() : 0;
-		result = 31 * result + (productionCode != null ? productionCode.hashCode() : 0);
-		result = 31 * result + (testCode != null ? testCode.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString () {
-		return "Specification{" +
-				"name='" + name + '\'' +
-				", productionCode='" + productionCode + '\'' +
-				", testCode='" + testCode + '\'' +
-				'}';
-	}
-
 	public Class getReturnType () {
 		return getReturnValue().getClass();
 	}
@@ -143,6 +111,38 @@ class Specification {
 			i++;
 		}
 		return parametersWithNames;
+	}
+
+	@Override
+	public boolean equals (final Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Specification)) return false;
+
+		final Specification that = (Specification) o;
+
+		if (name != null ? !name.equals(that.name) : that.name != null) return false;
+		if (productionCode != null ? !productionCode.equals(that.productionCode) : that.productionCode != null)
+			return false;
+		if (testCode != null ? !testCode.equals(that.testCode) : that.testCode != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode () {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (productionCode != null ? productionCode.hashCode() : 0);
+		result = 31 * result + (testCode != null ? testCode.hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString () {
+		return "Specification{" +
+				"name='" + name + '\'' +
+				", productionCode='" + productionCode + '\'' +
+				", testCode='" + testCode + '\'' +
+				'}';
 	}
 
 }
