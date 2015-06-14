6901b00b09a8a6160baf5f8c7b71dd55f5a43a73
alvaro garcia <derecerca@gmail.com>
2015-06-14 12:23:09 +0200

    refactor: remove before, remove redundant assert

--- src/test/java/com/example/spike/tdd/codegenerator/HypothesisSetShould.java
+++ src/test/java/com/example/spike/tdd/codegenerator/HypothesisSetShould.java
@@ -1,34 +1,24 @@
 package com.example.spike.tdd.codegenerator;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.function.Function;
 
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 
 public class HypothesisSetShould {
 
 	private HypothesisSet sut;
 
-	@Before
-	public void setUp () {
-		sut = new HypothesisSet(Arrays.asList(new Hypothesis(Arrays.asList(2), 2)));
-	}
-
 	@Test
 	public void find_the_identity () {
+		sut = new HypothesisSet(Arrays.asList(new Hypothesis(Arrays.asList(2), 2)));
 
 		final Object formula = sut.findFormula();
 
-		assertThat(formula, is(not(nullValue())));
-
 		assertThat(((Function) formula).apply(1), is(1));
-
 	}
 
 }
