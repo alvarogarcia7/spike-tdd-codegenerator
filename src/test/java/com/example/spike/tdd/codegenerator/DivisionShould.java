95afa2787d0e4ca689e4d8826cda68160c2a4919
alvaro garcia <derecerca@gmail.com>
2015-07-09 22:33:04 +0200

    refactor: move said factory method to an outside HypothesisBuilder

--- src/test/java/com/example/spike/tdd/codegenerator/DivisionShould.java
+++ src/test/java/com/example/spike/tdd/codegenerator/DivisionShould.java
@@ -6,17 +6,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.spike.tdd.codegenerator.HypothesisBuilder.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DivisionShould {
 
-
-
 	@Test
 	public void not_fail_if_the_first_output_is_zero () {
 
-		final Hypothesis hypothesis1 = buildHypothesis(0, 1);
+		final Hypothesis hypothesis1 = hypothesis(0, 1);
 		final List<Hypothesis> hypotheses = Arrays.asList(hypothesis1);
 
 		assertThat(new Division().find(hypotheses), is(Optional.empty()));
@@ -27,14 +26,11 @@ public class DivisionShould {
 
 		final int input1 = 1;
 		final int output1 = 1;
-		final Hypothesis hypothesis1 = buildHypothesis(output1, input1);
-		final Hypothesis hypothesis2 = buildHypothesis(0, input1);
+		final Hypothesis hypothesis1 = hypothesis(output1, input1);
+		final Hypothesis hypothesis2 = hypothesis(0, input1);
 		final List<Hypothesis> hypotheses = Arrays.asList(hypothesis1, hypothesis2);
 
 		assertThat(new Division().find(hypotheses), is(Optional.empty()));
 	}
 
-	private Hypothesis buildHypothesis (Object output, Object input) {
-		return new Hypothesis(Arrays.asList(input), output);
-	}
 }
