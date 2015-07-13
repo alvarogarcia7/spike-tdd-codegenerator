ab0d69ee04fd8ffdd9ceef99e69b0016a01bbcf4
alvaro garcia <derecerca@gmail.com>
2015-07-13 22:23:14 +0200

    refactor: apply last refactor to another class

--- src/main/java/com/example/spike/tdd/codegenerator/operation/Constant.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/Constant.java
@@ -1,24 +1,25 @@
 package com.example.spike.tdd.codegenerator.operation;
 
+import com.example.spike.tdd.codegenerator.SingleIntOperation;
 import com.example.spike.tdd.codegenerator.application.Application;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class Constant implements Operation {
+public class Constant extends SingleIntOperation{
 
 	@Override
 	public Optional<Function> find (final List<Application> hypotheses) {
-		final Object first = hypotheses.get(0).getOutput();
+		final Object first = getOutput(hypotheses);
 
 		for (Application current : hypotheses) {
-			if(!current.getOutput().equals(first)){
+			if (!current.getOutput().equals(first)) {
 				return Optional.empty();
 			}
 		}
 
-		final Function function =  (o) -> first;
+		final Function function = (o) -> first;
 		return Optional.of(function);
 	}
 }
