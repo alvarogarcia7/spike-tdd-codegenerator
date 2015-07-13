c1e8df9283de1093ddae29df22fb01bcf44a32c4
alvaro garcia <derecerca@gmail.com>
2015-07-13 22:22:03 +0200

    refactor: apply last refactor to another class

--- src/main/java/com/example/spike/tdd/codegenerator/operation/Difference.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/Difference.java
@@ -1,16 +1,17 @@
 package com.example.spike.tdd.codegenerator.operation;
 
+import com.example.spike.tdd.codegenerator.SingleIntOperation;
 import com.example.spike.tdd.codegenerator.application.Application;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class Difference implements Operation {
+public class Difference extends SingleIntOperation {
 	@Override
 	public Optional<Function> find (List<Application> hypotheses) {
-		final int input = (int)hypotheses.get(0).getParameters().get(0);
-		final int output = (int) hypotheses.get(0).getOutput();
+		final int input = firstInput(hypotheses);
+		final int output = getOutput(hypotheses);
 
 		final int increment = output - input;
 
