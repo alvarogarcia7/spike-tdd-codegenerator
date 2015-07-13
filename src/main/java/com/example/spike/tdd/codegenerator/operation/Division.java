658717563628dcdb58c5a5eb8ee899d03faecda2
alvaro garcia <derecerca@gmail.com>
2015-07-13 22:22:52 +0200

    refactor: apply last refactor to another class

--- src/main/java/com/example/spike/tdd/codegenerator/operation/Division.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/Division.java
@@ -1,22 +1,23 @@
 package com.example.spike.tdd.codegenerator.operation;
 
+import com.example.spike.tdd.codegenerator.SingleIntOperation;
 import com.example.spike.tdd.codegenerator.application.Application;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class Division implements Operation {
+public class Division extends SingleIntOperation {
 
 	@Override
 	public Optional<Function> find (final List<Application> hypotheses) {
-		final int a = (int) hypotheses.get(0).getParameters().get(0);
-		final int b = (int) hypotheses.get(0).getOutput();
-		if(hypotheses.stream().filter(x -> x.getOutput().equals(0)).findAny().isPresent()){
+		final int a = firstInput(hypotheses);
+		final int b = getOutput(hypotheses);
+		if (hypotheses.stream().filter(x -> x.getOutput().equals(0)).findAny().isPresent()) {
 			return NO_FUNCTION;
 		}
 		final int divisor = a / b;
-		if(divisor == 0){
+		if (divisor == 0) {
 			return NO_FUNCTION;
 		}
 		Function f = (x) -> (int) x / divisor;
