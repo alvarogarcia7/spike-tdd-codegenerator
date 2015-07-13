9c5f38a53d8fa6b9019ac6c20983d6d14d727d5d
alvaro garcia <derecerca@gmail.com>
2015-07-13 21:56:58 +0200

    red: verify_the_applications_against_all_the_operations

--- src/test/java/com/example/spike/tdd/codegenerator/OperationFinderShould.java
+++ src/test/java/com/example/spike/tdd/codegenerator/OperationFinderShould.java
@@ -2,15 +2,16 @@ package com.example.spike.tdd.codegenerator;
 
 import com.example.spike.tdd.codegenerator.application.Application;
 import com.example.spike.tdd.codegenerator.application.Applications;
+import com.example.spike.tdd.codegenerator.operation.Constant;
 import com.example.spike.tdd.codegenerator.operation.Difference;
 import com.example.spike.tdd.codegenerator.operation.Division;
-import com.example.spike.tdd.codegenerator.operation.Constant;
 import com.example.spike.tdd.codegenerator.operation.Operation;
 import com.example.spike.tdd.codegenerator.operation.OperationFinder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -18,6 +19,11 @@ import static com.example.spike.tdd.codegenerator.application.ApplicationBuilder
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class OperationFinderShould {
 
@@ -81,6 +87,22 @@ public class OperationFinderShould {
 		expectedException.expectMessage(is("Ambiguous function"));
 
 		findFormulaMatching(applicationFor(1, 1));
+	}
+
+	@Test
+	public void verify_the_applications_against_all_the_operations () {
+
+		final Operation operation1 = mock(Operation.class);
+		doReturn(Operation.NO_FUNCTION).when(operation1).find(anyList());
+		final Applications applications = applicationFor(1, 1);
+
+		try {
+			new OperationFinder(applications, Arrays.asList(operation1, operation1)).findOperation();
+		} catch (UnsupportedOperationException e) {
+			// no exception
+		}
+
+		verify(operation1, times(2)).find(applications.values());
 	}
 
 	private Applications applicationsFor (final int input1, final int output1, final int input2, final int output2) {
