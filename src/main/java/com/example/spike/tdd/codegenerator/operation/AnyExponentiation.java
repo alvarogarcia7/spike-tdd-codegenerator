fec456b49a9e6350836b63618b3cfb11e08320dc
alvaro garcia <derecerca@gmail.com>
2015-07-16 21:37:36 +0200

    refactor: extract method for readability

--- src/main/java/com/example/spike/tdd/codegenerator/operation/AnyExponentiation.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/AnyExponentiation.java
@@ -12,22 +12,21 @@ public class AnyExponentiation extends SingleIntOperation {
 	@Override
 	public Optional<Function> find (final List<Application> hypotheses) {
 
-		final List<Integer> exponentCandidates = new ArrayList<>();
-		for (int i = 0; i < hypotheses.size(); i++) {
-			final int input = getInput(hypotheses, i);
-			final int output = getOutput(hypotheses, i);
+		final List<Integer> exponentCandidates = getCandidateExponents(hypotheses);
 
-			if (input == output) {
-				continue;
-			} else {
-				for (int j = 0; j < output; j++) {
-					if (powAsInt(input, j) == output) {
-						exponentCandidates.add(j);
-					}
-				}
-			}
+		final List<Integer> matchingCandidates = filterAllThoseMatch(hypotheses, exponentCandidates);
+
+		if(matchingCandidates.size() == 1 ){
+			System.out.println(matchingCandidates);
+			return Optional.of((x) -> powAsInt((int) x, matchingCandidates.get(0)));
+		} else if (matchingCandidates.size() > 1){
+			System.out.println("Found more than one solution: AnyExponentiation, with exponents " + matchingCandidates);
 		}
 
+		return Optional.empty();
+	}
+
+	private List<Integer> filterAllThoseMatch (final List<Application> hypotheses, final List<Integer> exponentCandidates) {
 		final List<Integer> matchingCandidates = new ArrayList<>();
 		for (Integer exponentCandidate : exponentCandidates) {
 
@@ -46,15 +45,26 @@ public class AnyExponentiation extends SingleIntOperation {
 				matchingCandidates.add(exponentCandidate);
 			}
 		}
+		return matchingCandidates;
+	}
 
-		if(matchingCandidates.size() == 1 ){
-			System.out.println(matchingCandidates);
-			return Optional.of((x) -> powAsInt((int) x, matchingCandidates.get(0)));
-		} else if (matchingCandidates.size() > 1){
-			System.out.println("Found more than one solution: AnyExponentiation, with exponents " + matchingCandidates);
+	private List<Integer> getCandidateExponents (final List<Application> hypotheses) {
+		final List<Integer> exponentCandidates = new ArrayList<>();
+		for (int i = 0; i < hypotheses.size(); i++) {
+			final int input = getInput(hypotheses, i);
+			final int output = getOutput(hypotheses, i);
+
+			if (input == output) {
+				continue;
+			} else {
+				for (int j = 0; j < output; j++) {
+					if (powAsInt(input, j) == output) {
+						exponentCandidates.add(j);
+					}
+				}
+			}
 		}
-
-		return Optional.empty();
+		return exponentCandidates;
 	}
 
 	private int powAsInt (final int input, final int j) {
