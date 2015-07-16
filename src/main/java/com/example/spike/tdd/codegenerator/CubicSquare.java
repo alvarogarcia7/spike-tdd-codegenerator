5b82f9e8d28a0fb8d07879bb2246fa9cf9d85c12
alvaro garcia <derecerca@gmail.com>
2015-07-16 21:05:54 +0200

    green

--- src/main/java/com/example/spike/tdd/codegenerator/CubicSquare.java
+++ src/main/java/com/example/spike/tdd/codegenerator/CubicSquare.java
@@ -1,6 +1,26 @@
 package com.example.spike.tdd.codegenerator;
 
+import com.example.spike.tdd.codegenerator.application.Application;
 import com.example.spike.tdd.codegenerator.operation.Operation;
 
-public class CubicSquare implements Operation {
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class CubicSquare extends SingleIntOperation {
+	@Override
+	public Optional<Function> find (final List<Application> hypotheses) {
+		final int input = firstInput(hypotheses);
+		final int output = getOutput(hypotheses);
+
+		if(cubicSquare(input) == output) {
+			return Optional.of((x) -> cubicSquare((int) x));
+		} else {
+			return Operation.NO_FUNCTION;
+		}
+	}
+
+	private int cubicSquare (final int input) {
+		return (int)Math.pow(input, 3);
+	}
 }
