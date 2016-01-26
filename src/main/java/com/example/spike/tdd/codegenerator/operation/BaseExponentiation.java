622018b1ce93995a437d62e30e6cc9c40b92bd0b
alvaro garcia <derecerca@gmail.com>
2016-01-26 22:19:35 +0100

    mark this class with an interface

--- src/main/java/com/example/spike/tdd/codegenerator/operation/BaseExponentiation.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/BaseExponentiation.java
@@ -1,6 +1,6 @@
 package com.example.spike.tdd.codegenerator.operation;
 
-public class BaseExponentiation extends SearchSingleIntOperation {
+public class BaseExponentiation extends SearchSingleIntOperation implements Representable {
 
 	@Override
 	protected int apply (final int input, final int candidate) {
