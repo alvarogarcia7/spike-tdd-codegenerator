f0caaab7170b7acd771a9a23034849efd2686326
alvaro garcia <derecerca@gmail.com>
2016-01-26 21:59:46 +0100

    this is not deprecated

--- src/main/java/com/example/spike/tdd/codegenerator/operation/PowerOf.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/PowerOf.java
@@ -2,7 +2,6 @@ package com.example.spike.tdd.codegenerator.operation;
 
 public class PowerOf extends SearchSingleIntOperation {
 
-	@Deprecated
 	protected int apply (final int input, final int candidate) {
 		return (int) Math.pow(input, candidate);
 	}
