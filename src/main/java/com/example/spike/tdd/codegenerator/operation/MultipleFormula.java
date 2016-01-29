5a9550ccbd2bf4f5a449db874c5f91762d37a57e
alvaro garcia <derecerca@gmail.com>
2016-01-29 11:43:06 +0100

    green: generalize finding inclination and starting point

--- src/main/java/com/example/spike/tdd/codegenerator/operation/MultipleFormula.java
+++ src/main/java/com/example/spike/tdd/codegenerator/operation/MultipleFormula.java
@@ -5,6 +5,7 @@ import com.example.spike.tdd.codegenerator.application.Application;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MultipleFormula extends SingleIntOperation {
 	private final Operation[] operations;
@@ -15,11 +16,35 @@ public class MultipleFormula extends SingleIntOperation {
 
 	@Override
 	public Optional<Function> find (final List<Application> hypotheses) {
+		final List<Integer> inputParameters = hypotheses.stream()
+				.map(Application::getParameters).map(x -> (int) x.get(0))
+				.collect(Collectors.toList());
+		final List<Integer> results = hypotheses.stream()
+				.map(x -> (int) x.getOutput())
+				.collect(Collectors.toList());
+
+
+		if(!inputParameters.contains(0)){
+			throw new RuntimeException("Parameters must include function at x = 0");
+		}
+		assert inputParameters.contains(0);
+
+		Integer startingPoint = 0;
+		for (int i = 0; i < inputParameters.size(); i++) {
+			if (inputParameters.get(i) == 0) {
+				startingPoint = results.get(i);
+			}
+		}
+
+
+		final int incline = (results.get(1) - results.get(0)) / (inputParameters.get(1) - inputParameters.get(0));
+
 		final int a = firstInput(hypotheses);
 		final int b = getOutput(hypotheses);
 
-		final Function f = (x) -> (int) x * 2;
-		final Function g = f.andThen((x) -> (int) x + 1);
+		final Function f = (x) -> (int) x * incline;
+		final Integer finalStartingPoint = startingPoint;
+		final Function g = f.andThen((x) -> (int) x + finalStartingPoint);
 
 		if (g.apply(a).equals(b)) {
 			return Optional.of(g);
